e28ba7f69a8d6a207c66f1527f9027935f9abdb3
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-01-11 11:44:12 -0800

    提取语法分析部分, 准备单元测试

--- src/main/java/com/中文编程/圈5/主程序/运行器.java
+++ src/main/java/com/中文编程/圈5/主程序/运行器.java
@@ -18,15 +18,26 @@ public class 运行器 {
   private static final 词法错误监听器 词法错误处理 = new 词法错误监听器();
 
   public static void main(String[] 参数) {
-    CharStream 输入;
+    CharStream 输入流;
     try {
-      输入 = CharStreams.fromFileName(参数[0]);
+      输入流 = CharStreams.fromFileName(参数[0]);
     } catch (IOException e) {
       System.out.println(参数[0] + "文件打开出错");
       return;
     }
 
-    圈5Lexer 词法分析器 = new 圈5Lexer(输入);
+    节点 语法树 = 语法分析(输入流);
+    if (语法树 != null) {
+      System.out.println(语法树.get原始文本());
+    }
+  }
+
+  public static 节点 语法分析(String 源码文本) {
+    return 语法分析(CharStreams.fromString(源码文本));
+  }
+
+  public static 节点 语法分析(CharStream 源码输入流) {
+    圈5Lexer 词法分析器 = new 圈5Lexer(源码输入流);
     词法分析器.removeErrorListeners();
     词法分析器.addErrorListener(词法错误处理);
     圈5Parser 语法分析器 = new 圈5Parser(new CommonTokenStream(词法分析器));
@@ -34,10 +45,6 @@ public class 运行器 {
     语法分析器.addErrorListener(语法错误处理);
 
     定制访问器 访问器 = new 定制访问器();
-    节点 语法树 = 访问器.visit(语法分析器.程序());
-    if (语法树 != null) {
-      System.out.println(语法树.get原始文本());
-    }
-
+    return 访问器.visit(语法分析器.程序());
   }
 }
