01a7e1fee8e50a504aa57a3dac8d4314b86ddf1e
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-01-12 10:54:45 -0800

    通过变量赋值后求值测试

--- src/main/java/com/中文编程/圈5/分析器/定制访问器.java
+++ src/main/java/com/中文编程/圈5/分析器/定制访问器.java
@@ -1,12 +1,17 @@
 package com.中文编程.圈5.分析器;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 import com.中文编程.圈5.分析器.圈5Parser.乘除Context;
 import com.中文编程.圈5.分析器.圈5Parser.加減Context;
+import com.中文编程.圈5.分析器.圈5Parser.变量Context;
 import com.中文编程.圈5.分析器.圈5Parser.数Context;
 import com.中文编程.圈5.分析器.圈5Parser.求值Context;
+import com.中文编程.圈5.分析器.圈5Parser.赋值Context;
 import com.中文编程.圈5.语法树.数节点;
 import com.中文编程.圈5.语法树.节点;
 import com.中文编程.圈5.语法树.表达式节点;
@@ -14,9 +19,30 @@ import com.中文编程.圈5.语法树.运算符号;
 
 public class 定制访问器 extends 圈5BaseVisitor<节点> {
 
+  private static Map<String, 节点> 变量值表 = new HashMap<>();
+
+  // 以下为声明部分
+
+  @Override
+  public 节点 visit赋值(赋值Context 上下文) {
+    String 变量名 = 上下文.T变量名().getText();
+    变量值表.put(变量名, visit(上下文.表达式()));
+    return null;
+  }
+
   @Override
   public 节点 visit求值(求值Context 上下文) {
     return visit(上下文.表达式());
+  }
+
+  // 以下为表达式部分
+
+  @Override
+  public 节点 visit变量(变量Context 上下文) {
+    String 变量名 = 上下文.T变量名().getText();
+    
+    // TODO: 添加变量检查
+    return 变量值表.get(变量名);
   }
 
   @Override
